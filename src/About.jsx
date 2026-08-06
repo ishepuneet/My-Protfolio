7a6a23054c640b532fe733a2ccac8c88fbac6d05
ishepuneet <punu1122001@gmail.com>
2026-08-06 17:25:28 +0530

    port

--- src/About.jsx
+++ src/About.jsx
@@ -98,7 +98,7 @@
 // }
 
 import React, { useEffect, useState } from "react";
-import cv from "../public/img/resume.pdf";
+import cv from "../public/img/resume.docx";
 
 export default function About() {
   const [visibleSections, setVisibleSections] = useState({});
